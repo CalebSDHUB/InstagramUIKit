495d16a51f2e635ac105774594f809d4b8a26c1a
Caleb Danielsen <caleb@lfpost.dk>
2021-10-25 20:55:20 +0200

    Comments complete

--- InstagramCloneFirestore/Controller/FeedController.swift
+++ InstagramCloneFirestore/Controller/FeedController.swift
@@ -86,6 +86,7 @@ extension FeedController {
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: resuseIdentifier, for: indexPath) as! FeedCell
+        cell.delegate = self
         if let post = post {
             cell.viewModel = PostViewModel(post: post)
         } else {
@@ -94,6 +95,8 @@ extension FeedController {
         return cell
     }
 }
+
+// MARK: - UICollectionViewDelegateFlowLayout
 
 extension FeedController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -104,5 +107,15 @@ extension FeedController: UICollectionViewDelegateFlowLayout {
         height += 60
         
         return CGSize(width: width, height: height)
+    }
+}
+
+// MARK: - FeedCellDelegate
+
+extension FeedController: FeedCellDelegate {
+    
+    func cell(_ cell: FeedCell, wantsToShowCommentsFor post: Post) {
+        let controller = CommentController(post: post)
+        navigationController?.pushViewController(controller, animated: true)
     }
 }
