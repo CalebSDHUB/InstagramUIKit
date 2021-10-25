416e6498fc9aaa34a1bbb4308cd3826f51f3c1b8
Caleb Danielsen <caleb@lfpost.dk>
2021-10-25 23:20:52 +0200

    Likes complete

--- InstagramCloneFirestore/View/FeedCell.swift
+++ InstagramCloneFirestore/View/FeedCell.swift
@@ -9,6 +9,7 @@ import UIKit
 
 protocol FeedCellDelegate: AnyObject {
     func cell(_ cell: FeedCell, wantsToShowCommentsFor post: Post)
+    func cell(_ cell: FeedCell, didLike post: Post)
 }
 
 class FeedCell: UICollectionViewCell {
@@ -46,10 +47,11 @@ class FeedCell: UICollectionViewCell {
         return iv
     }()
     
-    private lazy var likeButton: UIButton = {
+    lazy var likeButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(#imageLiteral(resourceName: "like_unselected"), for: .normal)
         button.tintColor = .black
+        button.addTarget(self, action: #selector(didTapLike), for: .touchUpInside)
         return button
     }()
     
@@ -135,6 +137,11 @@ class FeedCell: UICollectionViewCell {
         delegate?.cell(self, wantsToShowCommentsFor: viewModel.post)
     }
     
+    @objc func didTapLike() {
+        guard let viewModel = viewModel else { return }
+        delegate?.cell(self, didLike: viewModel.post)
+    }
+    
     // MARK: - Helpers
     
     func configure() {
@@ -144,6 +151,8 @@ class FeedCell: UICollectionViewCell {
         profileImageView.sd_setImage(with: viewModel.userProfileImageUrl)
         usernameButton.setTitle(viewModel.username, for: .normal)
         likesLabel.text = viewModel.likesLabelText
+        likeButton.tintColor = viewModel.likeButtonTinColor
+        likeButton.setImage(viewModel.likeButtonImage, for: .normal)
     }
     
     private func configureActionButtons() {
--- InstagramCloneFirestore/ViewModel/PostViewModel.swift
+++ InstagramCloneFirestore/ViewModel/PostViewModel.swift
@@ -5,16 +5,21 @@
 //  Created by Caleb Danielsen on 22/10/2021.
 //
 
-import Foundation
+import UIKit
 
 struct PostViewModel {
-    let post: Post
+    var post: Post
     
     var imageUrl: URL? { return URL(string: post.imageUrl) }
     var userProfileImageUrl: URL? { return URL(string: post.ownerImageUrl)}
     var username: String { return post.ownerUsername}
     var caption: String { return post.caption }
     var likes: Int { return post.likes }
+    var likeButtonTinColor: UIColor { return post.didLike ? .red : .black }
+    var likeButtonImage: UIImage? {
+        let imageName = post.didLike ? "like_selected" : "like_unselected"
+        return UIImage(named: imageName)
+    }
     var likesLabelText: String {
         if post.likes != 1 {
             return "\(post.likes) likes"
