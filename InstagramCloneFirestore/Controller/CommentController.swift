d1d04b10b710b71f277fe681c954b2d3ac637b92
Caleb Danielsen <caleb@lfpost.dk>
2021-10-29 22:50:00 +0200

    Notifications complete

--- InstagramCloneFirestore/Controller/CommentController.swift
+++ InstagramCloneFirestore/Controller/CommentController.swift
@@ -123,13 +123,15 @@ extension CommentController: CommentInputAccessoryViewDelegate {
         inputView.clearCommentTextView()
         
         guard let tab = tabBarController as? MainTabController else { return }
-        guard let user = tab.user else { return }
+        guard let currentUser = tab.user else { return }
         
         showLoader(true)
         
-        CommentService.uploadComment(comment: comment, postID: post.postId, user: user) { (error) in
+        CommentService.uploadComment(comment: comment, postID: post.postId, user: currentUser) { [self] (error) in
             self.showLoader(false)
             inputView.clearCommentTextView()
+            
+            NotificationService.uploadNotification(toUid: post.ownerUid, fromUser: currentUser, type: .comment, post: post)
         }
     }
 }
--- InstagramCloneFirestore/View/FeedCell.swift
+++ InstagramCloneFirestore/View/FeedCell.swift
@@ -10,6 +10,7 @@ import UIKit
 protocol FeedCellDelegate: AnyObject {
     func cell(_ cell: FeedCell, wantsToShowCommentsFor post: Post)
     func cell(_ cell: FeedCell, didLike post: Post)
+    func cell(_ cell: FeedCell, wantsToShowProfileFor uid: String)
 }
 
 class FeedCell: UICollectionViewCell {
@@ -22,19 +23,23 @@ class FeedCell: UICollectionViewCell {
     
     weak var delegate: FeedCellDelegate?
     
-    private let profileImageView: UIImageView = {
+    private lazy var profileImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleToFill
         iv.clipsToBounds = true
         iv.isUserInteractionEnabled = true
         iv.backgroundColor = .lightGray
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(showUserProfile))
+        iv.isUserInteractionEnabled = true
+        iv.addGestureRecognizer(tap)
         return iv
     }()
     
     private lazy var usernameButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitleColor(.black, for: .normal)
-        button.addTarget(self, action: #selector(didTapUsername), for: .touchUpInside)
+        button.addTarget(self, action: #selector(showUserProfile), for: .touchUpInside)
         return button
     }()
     
@@ -128,8 +133,9 @@ class FeedCell: UICollectionViewCell {
     
     // MARK: - Actions
     
-    @objc func didTapUsername() {
-        print("DEBUG: did tap username")
+    @objc func showUserProfile() {
+        guard let viewModel = viewModel else { return }
+        delegate?.cell(self, wantsToShowProfileFor: viewModel.post.ownerUid)
     }
     
     @objc func didTapComments() {
